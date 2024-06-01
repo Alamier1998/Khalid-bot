39806fa507341a11868826426681e4e5955fac4c
Alamier1998 <smologistic1@gmail.com>
2024-06-01 18:09:02 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,10 +2,10 @@ from pyrogram import Client, filters
 import requests
 import random
 
-api_id = 26480985 #--Add your Api Id here
-api_hash = '56c935fae1c5c86ba5a3af655f8caa9d' #--Enter Api Hash Here
+api_id = 29729223 #--Add your Api Id here
+api_hash = 'f828bcc05b10ffdfdcf7fa1d5ca5cd8a' #--Enter Api Hash Here
 
-token = '7195594057:AAHfeGa1M11mAVFOo8SfUDYPZG-vIAx8_Ow' #--Enter Bot Token Here.
+token = '7083062463:AAFMx6JA1YH0rw8rXAeJCksGONw2GSz5h8E' #--Enter Bot Token Here.
 
 emojis = ["馃憤", "馃憥", "鉂わ笍", "馃敟", "馃グ", "馃憦", "馃榿", "馃", "馃く", "馃槺", "馃が", "馃槩", "馃帀", "馃ぉ", "馃ぎ", "馃挬", "馃檹", "馃憣", "馃晩", "馃ぁ", "馃ケ", "馃ゴ", "馃槏", "馃惓", "鉂も�嶐煍�", "馃寶", "馃尛", "馃挴", "馃ぃ", "鈿★笍", "馃崒", "馃弳", "馃挃", "馃え", "馃槓", "馃崜", "馃嵕", "馃拫", "馃枙", "馃槇", "馃槾", "馃槶", "馃", "馃懟", "馃懆鈥嶐煉�", "馃憖", "馃巸", "馃檲", "馃槆", "馃槰", "馃", "鉁嶏笍", "馃", "馃", "馃巺", "馃巹", "鈽冿笍", "馃拝", "馃お", "馃椏", "馃啋", "馃挊", "馃檳", "馃", "馃槝", "馃拪", "馃檴", "馃槑", "馃懢", "馃し鈥嶁檪", "馃し", "馃し鈥嶁檧", "馃槨"]
 
